c1ca2c636add945613582331c7803e3af3a9b6ee
Cole Miller <cole.t.miller@gmail.com>
2022-12-12 10:16:52 -0600

    added proper animation duration for footer

--- components/sections/Footer.js
+++ components/sections/Footer.js
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 const Footer = ({ t }) => {
   return (
-    <div className="w-full flex flex-col lg:flex-row lg:justify-between pt-[1rem] pb-[5rem] text-[.9rem] gap-[2rem] lg:gap-0 items-center text-center lg:text-left lg:items-start px-[3.2rem]">
+    <div className="w-full flex flex-col lg:flex-row lg:justify-between pt-[1rem] pb-[5rem] text-[.9rem] gap-[2rem] lg:gap-0 items-center text-center lg:text-left lg:items-start px-[3.2rem] bg-white dark:bg-black transition-all duration-700">
       <div>@ {t("genuineUndead")} 2023</div>
       <div>
         <ul>
--- components/sections/Navbar/Navbar.js
+++ components/sections/Navbar/Navbar.js
@@ -33,7 +33,7 @@ const Navbar = () => {
   }, [mounted]);
   return (
     <>
-      <nav className="w-min-screen h-[50px] top-0 sticky bg-white  dark:bg-black flex justify-between items-center transition-all duration-700 relative">
+      <nav className="w-min-screen h-[50px] top-0 sticky bg-white dark:bg-black flex justify-between items-center transition-all duration-700 relative">
         <div className="pl-[2.5rem] md:pl-[3.2rem]">
           <div className="z-101 w-[30px] h-[30px]">
             <GUIcon />
