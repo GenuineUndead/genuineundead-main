e2b9d72d650451993549e44b4414fb90e87e2362
Cole Miller <cole.t.miller@gmail.com>
2023-02-24 09:56:30 -0600

    testing home page spacing

--- components/sections/Home/Art.js
+++ components/sections/Home/Art.js
@@ -20,7 +20,7 @@ makes GU the best pixel art collection to ever exist.`;
 
 const Art = ({ t }) => {
   return (
-    <section className="min-h-[100vh] lg:min-h-screen w-full flex flex-col lg:flex-row lg:gap-[2.5rem] border-b border-black dark:border-white overflow-hidden">
+    <section className="min-h-[115vh] lg:min-h-[100vh] lg:min-h-screen w-full flex flex-col lg:flex-row lg:gap-[2.5rem] border-b border-black dark:border-white overflow-hidden">
       <div
         className={`w-full lg:w-[50%] lg:min-h-[50%] bg-[url('/images/background/golem-light.png')] dark:bg-[url('/images/background/golem.png')] bg-cover bg-center flex items-center justify-center flex-1`}
       >
