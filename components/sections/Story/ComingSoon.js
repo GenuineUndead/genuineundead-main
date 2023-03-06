97782a2664617a4dfc5a38e1cf3613d273317b6c
Cole Miller <cole.t.miller@gmail.com>
2023-03-06 16:12:08 -0600

    story page rough draft

--- components/sections/Story/ComingSoon.js
+++ components/sections/Story/ComingSoon.js
@@ -3,10 +3,10 @@ import React from "react";
 const ComingSoon = () => {
   return (
     <section className="min-h-screen w-full flex flex-col justify-center items-center border-b border-black dark:border-white pl-[1.6rem] lg:pl-[3.2rem] relative overflow-y-hidden ">
-      <h1 className="font-didot z-10  w-full text-center text-[15vw]">
-        <span className="font-teko font-bold text-[16.6vw]">GU</span>
-        <i>NIVERSE</i>
+      <h1 className="font-didot z-10 w-full text-center font-teko font-bold text-[20vw] md:text-[16.6vw]">
+        G <span className="text-[#8e8f8d]">UNIVERSE</span>
       </h1>
+      <p className=" text-[1.5rem] md:text-[1.75rem]">COMING SOON</p>
     </section>
   );
 };
