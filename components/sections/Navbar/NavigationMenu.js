1d31e9b0ea8db9f29053c8ea3396771cebe59a0d
Cole Miller <cole.t.miller@gmail.com>
2023-03-08 10:19:27 -0600

    updated nav with events

--- components/sections/Navbar/NavigationMenu.js
+++ components/sections/Navbar/NavigationMenu.js
@@ -12,6 +12,11 @@ const pageLinks = [
     path: "https://portal.genuineundead.io/collections/0x209e639a0ec166ac7a1a4ba41968fa967db30221",
     i18n: "portal",
   },
+  {
+    title: "EVENTS",
+    path: "https://events.genuineundead.io/genuine-undead",
+    i18n: "events",
+  },
   { title: "ART", path: "/art", i18n: "art" },
   { title: "STORY", path: "/story", i18n: "story" },
   { title: "COMMUNITY", path: "/community", i18n: "community" },
