4235606a4a4f108d9c05a646c1528ef68ef1f258
Cole Miller <cole.t.miller@gmail.com>
2023-03-22 21:07:40 -0500

    adding community meta description

--- pages/community.js
+++ pages/community.js
@@ -8,7 +8,9 @@ import Discord from "../components/sections/Community/Discord";
 import ReadMore from "../components/sections/Community/ReadMore";
 import SEO from "../components/SEO";
 
-const seoDesc = "Still Needed";
+const seoDesc =
+  "Genuine Undead is powered by the community. At every level it is the community who have stepped up, innovated and helped to magnify and harness the collective energy of our highly talented, diverse, experienced and powerful family. The GU collective have self organised into a fully councillor system to allow for a novel web 2.5 organisational approach to help merge traditional markets with the Web3 ecosystem. Blurring the lines between physical and digital.";
+
 const community = () => {
   const router = useRouter();
 
