374c25de4208a844ade519bd78e3874489a6cbb1
Cole Miller <cole.t.miller@gmail.com>
2023-03-28 14:47:23 -0500

    updated i want you image and copy

--- components/sections/Ethos/IWantYou.js
+++ components/sections/Ethos/IWantYou.js
@@ -3,7 +3,8 @@ import React from "react";
 const IWantYou = () => {
   return (
     <>
-      <section className="h-[550px] lg:h-[500px] xxl:h-[900px] w-full flex border-b border-black dark:border-white pl-[1.6rem] lg:pl-[3.2rem] bg-[url('/images/community/BornGenuine.png')] bg-no-repeat bg-cover xl:bg-fixed bg-center "></section>
+      <section className=" hidden lg:flex lg:h-[550px] xxl:h-[900px] w-full border-b border-black dark:border-white pl-[1.6rem] lg:pl-[3.2rem] bg-[url('/images/community/BornGenuine.png')] bg-no-repeat bg-cover bg-fixed bg-center"></section>
+      <section className="h-[600px] lg:hidden w-screen border-b border-black dark:border-white bg-[url('/images/community/BornGenuine.png')] bg-no-repeat bg-cover bg-center"></section>
     </>
   );
 };
