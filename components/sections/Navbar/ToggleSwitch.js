489d1eb865ffc95bde7bee8290da1317b6ad215e
Cole Miller <cole.t.miller@gmail.com>
2023-03-28 12:27:53 -0500

    adding for toggleSwitch to render based on theme change

--- components/sections/Navbar/ToggleSwitch.js
+++ components/sections/Navbar/ToggleSwitch.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const ToggleSwitch = ({ toggleTheme, theme }) => {
+  useEffect(() => {}, [theme]);
   return (
     <div className="flex items-center justify-center w-full gap-[5px]">
       <span>Light</span>
