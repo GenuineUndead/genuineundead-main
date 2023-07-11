bf3843e646ddd05702e33a0387486e1b960a9dfc
Cole Miller <cole.t.miller@gmail.com>
2023-07-11 10:58:32 -0500

    updated

--- components/sections/Community/Initiatives.js
+++ components/sections/Community/Initiatives.js
@@ -8,7 +8,7 @@ const Initiatives = () => {
     <section className="min-h-[110vh] md:min-h-[100vh] lg:min-h-[125vh] w-full flex flex-col lg:flex-row border-b border-black dark:border-white px-[1.6rem] lg:px-0 relative overflow-y-hidden inits-container">
       <div className="w-full min-h-full flex flex-col items-center justify-center gap-[1rem] positionCenter lg:my-[50px]">
         <h2
-          className={`text-[2.3rem] md:text-[3rem] lg:text-[4.0rem] xl:text-[4.5rem] xxl:text-[5.5rem] leading-none font-didot flex flex-col my-[20px]`}
+          className={`text-[2.3rem] md:text-[3rem] lg:text-[4.0rem] xl:text-[4.5rem] xxl:text-[5.5rem] leading-none font-didot flex flex-col mt-[20px] mb-[50px]`}
         >
           {" "}
           <span className="text-center md:whitespace-nowrap z-10">
