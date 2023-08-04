cd36d3363f70a12a76c1ea0b6412eab2ec513012
Cole Miller <cole.t.miller@gmail.com>
2023-08-04 07:19:58 -0500

    updated the openimage

--- components/SEO.js
+++ components/SEO.js
@@ -15,7 +15,7 @@ const SEO = ({ title, description, imagePath, path }) => {
         description: description,
         images: [
           {
-            url: "/GU-BANNER2.jpeg",
+            url: "/GU-BANNER.png",
             width: 1500,
             height: 500,
             alt: "Og Image Alt",
