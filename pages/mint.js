5396ca0b06c5880d482b856539f7fd27f4576863
Cole Miller <cole.t.miller@gmail.com>
2023-08-04 07:14:25 -0500

    updated main logo with tm

--- pages/mint.js
+++ pages/mint.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import MintMenu from "../components/sections/Mint/MintMenu";
+import Mint from "../components/sections/Mint/Mint";
 import SEO from "../components/SEO";
 
 const seoDesc = "Description of the mint page";
@@ -9,6 +10,7 @@ const mint = () => {
     <>
       <SEO title="Mint | Genuine Undead" description={seoDesc} path="mint" />
       <MintMenu />
+      <Mint />
     </>
   );
 };
