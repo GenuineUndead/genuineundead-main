14276e187c7c163fe6c6f390484906c9e3775c89
cole miller <cole.t.miller@gmail.com>
2023-08-12 16:56:50 -0500

    added timeline to nav

--- components/sections/Navbar/NavigationMenu.js
+++ components/sections/Navbar/NavigationMenu.js
@@ -17,11 +17,7 @@ const pageLinks = [
   { title: "COMMUNITY", path: "/community", i18n: "community" },
   { title: "ETHOS", path: "/ethos", i18n: "ethos" },
   { title: "STORY", path: "/story", i18n: "story" },
-  // {
-  //   title: "MINT",
-  //   path: "/mint",
-  //   i18n: "mint",
-  // },
+  { title: "TIMELINE", path: "/timeline", i18n: "timeline" },
   { title: "HOME", path: "/", i18n: "home" },
 ];
 
