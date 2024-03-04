4d84c32c12da6e6bf5cf390be162a210052394a6
cole miller <cole.t.miller@gmail.com>
2024-03-04 09:09:45 -0600

    updating redirect destination

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     return [
       {
         source: "/:path*", // Matches all paths
-        destination: "https://genuineundead.com/:path*", // Redirects to the external URL, preserving the path
+        destination: "https://www.genuineundead.com/:path*", // Redirects to the external URL, preserving the path
         permanent: true, // false for temporary redirect (307), true for permanent (308)
       },
     ];
